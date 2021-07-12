e4bfae5958b9c80bc2430506f923d6911bef902f
Rafael1799 <61852715+Rafael1799@users.noreply.github.com>
2021-07-12 01:07:01 -0300

    Create cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Fomulário de cadastro</title>
+</head>
+<body>
+	<h1>Formulário de cadastro</h1>
+	<form method= "get" action= "grava.php">
+		<input type="text" name="fnome" placeholder="NOME"><br>
+		<input type="text" name="femail" placeholder="EMAIL"><br>
+		<textarea name="fmsg" rows="5" cols="46" placeholder="MENSAGEM"></textarea><br>
+		<input type="submit" value="GRAVAR">
+		</form>
+</body>
+</html>
